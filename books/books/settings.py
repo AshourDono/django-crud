0e19e17578e1993c367c698fec2568867110376b
AshourDono <75224277+AshourDono@users.noreply.github.com>
2022-04-21 22:45:47 +0200

    configuration

--- books/books/settings.py
+++ books/books/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+from pickle import FALSE
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+igdo9k5i=a4q8olenn*l71mgt4j@y=9nd3=me5$)^vf2=^w*4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-iti-lab3.herokuapp.com/','127.0.0.1']
 
 
 # Application definition
